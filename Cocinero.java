3e09954aa120b6f7dee728fdfd728a2215490b0f
POO5 <santiago.vargas@epn.edu.ec>
2025-05-01 07:40:57 -0500

    semana3

--- Cocinero.java
+++ Cocinero.java
@@ -1,8 +1,18 @@
 public class Cocinero {
     String nombre;
-    String experiencia;
+    int experiencia;
     String especialidad;
-    public Cocinero() {
+    public Cocinero(String nombre,int experiencia,String especialidad) {
+        this.nombre = nombre;
+        this.experiencia = experiencia;
+        this.especialidad = especialidad;
+
+    }
+    public void mostrar() {
+        System.out.println("Nombre: "+nombre);
+        System.out.println("Años de experiencia: "+experiencia);
+        System.out.println("Especialidad: "+especialidad);
+
 
     }
 }
--- Main.java
+++ Main.java
@@ -1,6 +1,47 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String nombre1, nombre2, nombre3;
+        int experiencia1, experiencia2, experiencia3;
+        String especialidad1, especialidad2, especialidad3;
 
+
+        System.out.println("Nombre1: ");
+        nombre1 = scanner.nextLine();
+        System.out.println("Experiencia1: ");
+        experiencia1 = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Especialidad1: ");
+        especialidad1 = scanner.nextLine();
+        Cocinero c1 = new Cocinero(nombre1, experiencia1, especialidad1);
+
+
+        System.out.println("Nombre2: ");
+        nombre2 = scanner.nextLine();
+        System.out.println("Experiencia2: ");
+        experiencia2 = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Especialidad2: ");
+        especialidad2 = scanner.nextLine();
+        Cocinero c2 = new Cocinero(nombre1, experiencia1, especialidad1);
+
+
+        System.out.println("Nombre3: ");
+        nombre3 = scanner.nextLine();
+        System.out.println("Experiencia3: ");
+        experiencia3 = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Especialidad3: ");
+        especialidad3 = scanner.nextLine();
+        Cocinero c3 = new Cocinero(nombre1, experiencia1, especialidad1);
+
+        c1.mostrar();
+        c2.mostrar();
+        c3.mostrar();
+
+    scanner.close();
     }
+
 }
 //Santiago Vargas
